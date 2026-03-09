8a418c7beb18c8cb49b755e305a2dc75bba64b0d
BMPZXC <2298803971@qq.com>
2026-03-09 13:02:20 +0800

    Update 怪物.gd

--- 怪物/怪物.gd
+++ 怪物/怪物.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 class_name 怪物
 
 signal 死亡_
-var 生命:int:
+var 生命:float:
 	set(a):
 		生命=a
 		if a<=0:死亡_.emit()
 
 signal 受伤_ 
 ##如需复杂逻辑,请继承覆盖
-func 受击(伤害:int):
+func 受击(伤害:float):
 	if 伤害>0:受伤_.emit()
 	生命-=伤害
