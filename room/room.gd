116275849d0f18e57f0d3c9f50f566c4c77c7814
yuki <ganlinnian520@gmail.com>
2026-03-28 13:18:29 +0800

    Add files via upload

--- room/room.gd
+++ room/room.gd
@@ -1,33 +1,56 @@
 extends Node2D
 
-# 在检查器里把你的怪物场景（.tscn）拖进去
+# 设定怪物场景
 @export var enemy_scenes: Array[PackedScene] = []
-# 设定这个房间生多少只怪
-@export var enemy_count: int = 4
+# 设定怪物数量
+@export var enemy_count: int 
+# 限制每种怪物生成数量
+@export var max_per_type: int
+# 防止反复刷怪
+var has_triggered: bool = false
 
 func _ready() -> void:
-	spawn_enemies()
+	pass
+func _on_spawn_trigger_body_entered(body: Node2D) -> void:
+	#检查玩家信号
+	if not has_triggered and body.is_in_group("player"):
+		spawn_enemies()
+		# 标记为已触发
+		has_triggered = true 
 
 func spawn_enemies():
-	# 1. 获取所有标记为生成点的节点
+	# 获取所有标记为生成点的节点
 	var spawn_points = get_tree().get_nodes_in_group("enemy_spawn_point")
-	
-	# 2. 将点位顺序随机打乱
+	#  将点位顺序随机打乱
 	spawn_points.shuffle()
+	# 生成字典
+	var spawn_tracker = {}
+	#  确定生成数量
+	var actual_count = min(enemy_count, spawn_points.size())
 	
-	# 3. 确定生成数量（不能超过现有标记点总数）
-	var actual_spawn_count = min(enemy_count, spawn_points.size())
-	
-	# 4. 循环生成
-	for i in range(actual_spawn_count):
-		# 随机选一个点
-		var point = spawn_points[i]
-		# 随机选一种怪
-		var random_enemy_scene = enemy_scenes.pick_random()
+	for i in range(actual_count):
+		# 2. 筛选出目前还没达到上限的怪物种类
+		var available_monsters = []
+		for scene in enemy_scenes:
+			# 如果这种怪还没生成过，或者生成数量小于上限
+			if not spawn_tracker.has(scene) or spawn_tracker[scene] < max_per_type:
+				available_monsters.append(scene)
 		
-		# 实例化怪物
-		var enemy = random_enemy_scene.instantiate()
-		# 设置坐标
+		# 3. 到达s上限，停止生成
+		if available_monsters.is_empty():
+			break
+			
+		# 4. 从可选的列表里随机选一个
+		var point = spawn_points[i]
+		var chosen_scene = available_monsters.pick_random()
+		
+		# 5. 更新字典里的计数
+		if not spawn_tracker.has(chosen_scene):
+			spawn_tracker[chosen_scene] = 1
+		else:
+			spawn_tracker[chosen_scene] += 1
+			
+		# 6. 实例化并放置
+		var enemy = chosen_scene.instantiate()
 		enemy.global_position = point.global_position
-		# 将怪物添加进场景（建议添加给当前房间节点）
 		add_child(enemy)
